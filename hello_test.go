17039669646b281e5aebfe5c394424a6a9048de3
Amy Balmforth <amy.balmforth@gmail.com>
2020-01-29 15:51:11 +0000

    test for french

--- hello_test.go
+++ hello_test.go
@@ -29,4 +29,10 @@ func TestHello(t *testing.T) {
     assertCorrectMessage(t, got, want)
   })
 
+  t.Run("in French", func(t *testing.T) {
+    got := Hello("Pierre", "French")
+    want := "Bonjour, Pierre"
+    assertCorrectMessage(t, got, want)
+  })
+
 }
